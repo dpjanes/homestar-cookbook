655f5f59ab39916ae57398703a981fdfe8a0598e
David Janes <davidjanes@davidjanes.com>
2014-12-30 19:40:31 -0500

    some initial demo stuff

---
+++ messages/repeat-3.js
@@ -0,0 +1,29 @@
+/*
+ *  repeat-3.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2014-12-30
+ *
+ *  This will broadcast Count=0, Count=1, Count=3
+ *  when pushed.
+ */
+
+var homestar = require('homestar');
+
+homestar.recipe({
+    group: "Demo",
+    name: "Repeat 3",
+    run: function(context) {
+        context.message("");
+
+        var count = 0;
+        var id = setInterval(function() {
+            context.message("Count=" + count);
+            if (count++ >= 3) {
+                clearInterval(id);
+                context.done();
+            }
+        }, 3000);
+    }
+});
---
+++ messages/repeat-forever.js
@@ -0,0 +1,26 @@
+/*
+ *  repeat-forever.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2014-12-30
+ *
+ *  This will broadcast Count=0, Count=1, Count=3
+ *  and so on when pushed
+ */
+
+var homestar = require('homestar');
+
+homestar.recipe({
+    group: "Demo",
+    name: "Repeat Forever",
+    run: function(context) {
+        context.message("");
+
+        var count = 0;
+        setInterval(function() {
+            context.message("Count=" + count);
+            count++;
+        }, 3000);
+    }
+});
